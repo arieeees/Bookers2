436bf51e26b8e6e4689e2e5a05be096ea53d2184
arieeees <105973830+arieeees@users.noreply.github.com>
2022-07-13 11:43:55 +0000

    [Add]データベース上に必要なテーブルを作成

--- Bookers2/config/environments/development.rb
+++ Bookers2/config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "6466d21b1452497ebd9e0de81ecd4af0.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  
 end
