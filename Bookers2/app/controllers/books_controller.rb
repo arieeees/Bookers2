fa54297fa17db6bf0cbcd5c1e02fc112b64c8d35
arieeees <105973830+arieeees@users.noreply.github.com>
2022-07-31 13:41:19 +0000

    [Add]各ページ機能の追加

--- Bookers2/app/controllers/books_controller.rb
+++ Bookers2/app/controllers/books_controller.rb
@@ -1,8 +1,37 @@
 class BooksController < ApplicationController
+  
   def show
+    @newbook = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+        render "edit"
+    else
+        redirect_to books_path
+    end
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice]="Book was successfully destroyed."
+      redirect_to books_path
+    end
   end
 
   def index
--- Bookers2/config/routes.rb
+++ Bookers2/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'devise/sessions',
+    registrations: 'devise/registrations'
+  }  
+  
   root to: 'homes#top'
   
   devise_scope :user do
